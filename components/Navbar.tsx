d852ed003ec88caf00eec37dc1b684a48ac905a3
Nayan Suter <nayankbps@gmail.com>
2026-08-01 22:21:34 +0600

    implemented dark and light theme toggle

---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { Map, MessageSquare, Sparkle } from "lucide-react";
+import Link from "next/link";
+import { ThemeToggle } from "./theme-toggle";
+
+
+export default function Navbar() {
+  return (
+    <nav className="border-b bg-background">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <div className="flex items-center gap-6">
+          <Link href="/">
+            <div className="flex items-center gap-4">
+              <div className="flex items-center justify-center w-8 h-8 rounded-lg bg-linear-to-r from-purple-500 to-emerald-500">
+                <Sparkle className="w-6 h-6 text-white" />
+              </div>
+              <span className="text-xl font-semibold">Feedback Fusion</span>
+            </div>
+          </Link>
+           <Link
+            href="/roadmap"
+            className="text-sm hover:text-primary flex items-center gap-1"
+          >
+            <Map className="h-4 w-4" />
+            Roadmap
+          </Link>
+          <Link
+            href="/feedback"
+            className="text-sm hover:text-primary flex items-center gap-1"
+          >
+            <MessageSquare className="h-4 w-4" />
+            Feedback
+          </Link>
+          <div className="flex items-center gap-4">
+            <ThemeToggle/>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
