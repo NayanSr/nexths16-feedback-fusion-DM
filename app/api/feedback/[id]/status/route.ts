4630f075d0970ebe03aa91597248ff9da2da3fab
Nayan Suter <nayankbps@gmail.com>
2026-08-12 12:04:18 +0600

    api created for feedback and vote

---
+++ app/api/feedback/[id]/status/route.ts
@@ -0,0 +1,59 @@
+
+import { STATUS_ORDER } from "@/app/data/status-data";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: number }> }) {
+    try {
+        const { userId } = await auth();
+        console.log("AUTH", auth)
+        if (!userId) {
+            return NextResponse.json({ error: "Unaauthorized" }, { status: 401 })
+        }
+
+        // Check is user admin
+        const user = await prisma.user.findUnique({
+            where: {
+                clerkUserId: userId
+            }
+        })
+        if (!user || user.role !== "admin") {
+            return NextResponse.json(
+                {
+                    error: "Admin access required"
+                },
+                {
+                    status: 403
+                }
+            )
+        }
+
+        const { status } = await request.json();
+        const { id: postId } = await params;
+
+        //    Validate status
+        if (!STATUS_ORDER.includes(status)) {
+            return NextResponse.json({ error: "Invalide Status" }, { status: 400 })
+        }
+
+        const updatedPost = await prisma.post.update({
+            where: { id: postId },
+            data: {
+                status
+            },
+            include: {
+                author: true,
+                votes: true
+            }
+        })
+        return NextResponse.json(updatedPost)
+
+
+    } catch (error) {
+        console.error("Error updating post status", error);
+        return NextResponse.json({
+            error: "Internal Server Error"
+        }, { status: 500 })
+    }
+}
---
+++ app/api/votes/route.ts
@@ -0,0 +1,56 @@
+import prisma from "@/lib/prisma";
+import { syncCurrentUser } from "@/lib/sync-user";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        const dbUser = await syncCurrentUser();
+        if (!dbUser) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+        }
+        const { postId } = await request.json();
+        if (!postId) {
+            return NextResponse.json({
+                error: "Post ID is required"
+            },
+                { status: 400 })
+        }
+
+        // Check id vote already exist
+        const existingVote = await prisma.vote.findUnique({
+            where: {
+                userId_postId: {
+                    userId: dbUser.id,
+                    postId
+                }
+            }
+        }
+
+        )
+        if (existingVote) {
+            // Remove vote {toggle}
+            await prisma.vote.delete({
+                where: {
+                    id: existingVote.id
+                }
+            })
+            return NextResponse.json({ voted: false })
+
+        } else {
+            // Add vote {toggle}
+            await prisma.vote.create({
+                data: {
+                    userId: dbUser.id,
+                    postId
+                }
+            })
+            return NextResponse.json({ voted: true })
+        }
+
+    } catch (error) {
+        console.error("Error toggling vote", error);
+        return NextResponse.json({
+            error: "Internal Server Error"
+        }, { status: 500 })
+    }
+}
